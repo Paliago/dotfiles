51f798c391027d23aef5ba7a41b7ca30ad77d81e
Alvin Johansson <alvin@donedev.se>
2024-05-27 16:08:49 +0200

    chore: update lvim conf

--- .config/config.lua
+++ .config/config.lua
@@ -1,28 +1,16 @@
--- general
-lvim.log.level = "warn"
-lvim.format_on_save.enabled = false
-lvim.colorscheme = "catppuccin-mocha"
-
-vim.opt.number = true
 vim.opt.relativenumber = true
-
-lvim.format_on_save.enabled = true
+vim.opt.wrap = true
 
 vim.opt.guifont = "FiraCode Nerd Font Mono:h15"
-
-vim.g.neovide_input_macos_alt_is_meta = true
-vim.g.neovide_remember_window_size = true
-
--- keymappings [view all the defaults by pressing <leader>Lk]
-lvim.leader = "space"
 
 -- split
 lvim.keys.normal_mode["|"] = ":vsplit<CR>"
 lvim.keys.normal_mode["-"] = ":split<CR>"
 
 -- movement
-lvim.keys.normal_mode["<C-d>"] = "<C-d>zz"
-lvim.keys.normal_mode["<C-u>"] = "<C-u>zz"
+lvim.keys.normal_mode["<C_d>"] = "<C_d>zz"
+lvim.keys.normal_mode["<C_u>"] = "<C_u>zz"
+
 
 -- Spectre
 lvim.keys.normal_mode["<leader>S"] = '<cmd>lua require("spectre").toggle()<CR>'
@@ -30,183 +18,55 @@ lvim.keys.normal_mode["<leader>sw"] = '<cmd>lua require("spectre").open_visual({
 lvim.keys.visual_mode["<leader>sw"] = '<esc><cmd>lua require("spectre").open_visual()<CR>'
 lvim.keys.normal_mode["<leader>sp"] = '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>'
 
--- Project
-lvim.keys.normal_mode["<leader>P"] = "<cmd>lua require'telescope'.extensions.project.project{}<CR>"
-
--- Copilot
--- lvim.keys.normal_mode["<leader>C"] = '<cmd>lua require("copilot_panel").accept(){}<CR>'
-
--- TODO: User Config for predefined plugins
--- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.alpha.active = true
-lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.setup.view.side = "right"
-lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
-
--- if you don't want all the parsers change this to a table of the ones you want
-lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "c",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "typescript",
-  "tsx",
-  "css",
-  "rust",
-  "java",
-  "yaml",
-}
-
-lvim.builtin.treesitter.ignore_install = { "haskell" }
-lvim.builtin.treesitter.highlight.enable = true
-
--- set a formatter, this will override the language server formatting capabilities (if it exists)
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-  {
-    command = "prettier",
-    ---@usage arguments to pass to the formatter
-    -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-    -- extra_args = { "--print-with", "100" },
-    ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-    -- filetypes = { "typescript", "typescriptreact" },
-  },
-}
-
-lvim.builtin.telescope.on_config_done = function(telescope)
-  -- pcall(telescope.load_extension, "frecency")
-  -- pcall(telescope.load_extension, "neoclip")
-  pcall(telescope.load_extension, "project")
-  pcall(telescope.load_extension, "ui-select")
-  -- any other extensions loading
-end
-
--- Additional Plugins
 lvim.plugins = {
+  -- theme
   {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        integrations = {
-          alpha = true,
-          mason = true,
-          noice = true,
-          dap_ui = true,
-          nvimtree = true,
-          ts_rainbow2 = true,
-          window_picker = true,
-          telescope = true,
-          which_key = true,
-          cmp = true,
-          gitsigns = true,
-          treesitter = true,
-          notify = true,
-          mini = {
-            enabled = true,
-            indentscope_color = "",
-          },
+    opts = {
+      integrations = {
+        alpha = true,
+        mason = true,
+        noice = true,
+        dap_ui = true,
+        nvimtree = true,
+        ts_rainbow2 = true,
+        window_picker = true,
+        telescope = true,
+        which_key = true,
+        cmp = true,
+        gitsigns = true,
+        treesitter = true,
+        notify = true,
+        mini = {
+          enabled = true,
+          indentscope_color = "",
         }
-      })
-    end,
+      }
+    }
   },
-  {
-    "christoomey/vim-tmux-navigator",
-    cmd = {
-      "TmuxNavigateLeft",
-      "TmuxNavigateDown",
-      "TmuxNavigateUp",
-      "TmuxNavigateRight",
-      "TmuxNavigatePrevious",
-    },
-    keys = {
-      { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
-      { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
-      { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
-      { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
-      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-    },
-  },
-  {
-    "HiPhish/nvim-ts-rainbow2",
-  },
+
+  -- search and replace
   {
     "windwp/nvim-spectre",
     event = "BufRead",
-    config = function()
-      require("spectre").setup()
-    end,
   },
-  {
-    "nvim-telescope/telescope-project.nvim",
-    event = "BufWinEnter",
-    config = function()
-      require("telescope").load_extension("project")
-    end,
-  },
-  {
-    "folke/todo-comments.nvim",
-    event = "BufRead",
-    config = function()
-      require("todo-comments").setup()
-    end,
-  },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-  },
+
+  -- copilot
   {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
     event = "InsertEnter",
-    config = function()
-      require("copilot").setup({
-        -- inline suggestion
-        -- suggestion = {
-        --   enabled = true,
-        --   auto_trigger = true
-        -- }
-      })
-    end,
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+    }
   },
   {
     "zbirenbaum/copilot-cmp",
-    config = function()
-      require("copilot_cmp").setup({
-        suggestion = { enabled = true },
-        panel = { enabled = false }
-      })
-    end
+    opts = {
+      fix_pairs = true
+    }
   },
-  {
-    "rmagatti/auto-session",
-    config = function()
-      require("auto-session").setup({
-        auto_session_suppress_dirs = {
-          "~/",
-          "~/code",
-          "~/Downloads",
-          "~/code/elva",
-          "~/code/smart/",
-          "~/code/derome",
-          "~/code/perosnal",
-        },
-        session_lens = {
-          -- If load_on_setup is set to false, one needs to eventually call `require("auto-session").setup_session_lens()` if they want to use session-lens.
-          buftypes_to_ignore = {}, -- list of buffer types what should not be deleted from current session
-          load_on_setup = true,
-          theme_conf = { border = true },
-          previewer = false,
-        },
-        vim.keymap.set("n", "<leader>E", require("auto-session.session-lens").search_session, {
-          noremap = true,
-        })
-      })
-    end,
-  }
 }
-
-lvim.builtin.treesitter.rainbow.enable = true
